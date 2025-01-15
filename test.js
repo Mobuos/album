19f41ee1a21a90f245bb42fcf30afa11b3513bd6
João Dini <copperbrjdmdini@gmail.com>
2025-01-15 12:27:24 -0300

    test: simple counter api

--- test.js
+++ test.js
@@ -1,12 +1,46 @@
-const server = require('./backend/server.js');
-const supertest = require('supertest');
+import supertest from 'supertest';
+import server from './backend/server.js';
+import { expect } from "chai";
+
 const requestWithSupertest = supertest(server);
 
-describe('Teste', () => {
-    it('GET /test should have a message', async () => {
+describe('Simple API Test', () => {
+    it('GET /test should return a hello message', async () => {
         const res = await requestWithSupertest.get('/test');
-        expect(res.status).toEqual(200);
-        expect(res.type).toEqual(expect.stringContaining('json'));
-        expect(res.body).toHaveProperty('message')
+        expect(res.status).to.equal(200);
+        expect(res.type).to.include('json');
+        expect(res.body).to.have.property('message');
+        expect(res.body.message).to.include('Hello');
+        console.debug(res.body);
+    });
+});
+
+describe('Test Counter API', () => {
+    it('GET /counter should return initial value of 0', async () => {
+        const res = await requestWithSupertest.get('/counter');
+        expect(res.status).to.equal(200);
+        expect(res.type).to.include('json');
+        expect(res.body).to.have.property('value', 0);
+    });
+    it('POST /counter should increase value of counter by one', async () => {
+        const res = await requestWithSupertest.post('/counter');
+        expect(res.status).to.equal(200);
+        expect(res.type).to.include('json');
+        expect(res.body).to.have.property('value', 1);
+    });
+    it('multiple POST /counter should increase the counter accordingly', async () => {
+        const initialres = await requestWithSupertest.get('/counter');
+        var initialCounter = initialres.body.value;
+
+        const numRequests = 3;
+
+        for (let i = 0; i < numRequests; i++) {
+            await requestWithSupertest.post('/counter');
+        }
+
+        const res = await requestWithSupertest.get('/counter');
+        expect(res.status).to.equal(200);
+        expect(res.type).to.include('json');
+        expect(res.body).to.have.property('value', initialCounter + numRequests);
     });
 });
